e430f7adaf005bee04027dc706a79dca88c80be9
yuwchen <kuluychen@gmail.com>
2023-03-06 21:05:40 -0500

    Add files via upload

---
+++ evaluation_metric.py
@@ -0,0 +1,24 @@
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+import numpy as np
+
+
+def concordance_correlation_coefficient(y_true, y_pred,
+                       sample_weight=None,
+                       multioutput='uniform_average'):
+    cor=np.corrcoef(y_true,y_pred)[0][1]
+    
+    mean_true=np.mean(y_true)
+    mean_pred=np.mean(y_pred)
+    
+    var_true=np.var(y_true)
+    var_pred=np.var(y_pred)
+    
+    sd_true=np.std(y_true)
+    sd_pred=np.std(y_pred)
+    
+    numerator=2*cor*sd_true*sd_pred
+    
+    denominator=var_true+var_pred+(mean_true-mean_pred)**2
+
+    return numerator/denominator
